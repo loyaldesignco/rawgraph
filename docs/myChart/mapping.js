e180b29ca261e1e2bc27d1a42cc9cd21792b1016
Brian P <51056909+loyaldesignco@users.noreply.github.com>
2024-07-09 19:16:10 -0400

    Update mapping.js

--- docs/myChart/mapping.js
+++ docs/myChart/mapping.js
@@ -1,5 +1,7 @@
-export const mapData = {
-  x: 'get',
-  y: 'get',
-  color: 'get',
+export const mapData = function (data, mapping) {
+  return data.map((d) => ({
+    start: d[mapping.start.value],
+    duration: d[mapping.duration.value],
+    category: d[mapping.category.value],
+  }))
 }
