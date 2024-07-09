e55a0e818ec9edb23568714c128c20e34ffa662d
Brian P <51056909+loyaldesignco@users.noreply.github.com>
2024-07-09 19:15:25 -0400

    Update metadata.js

--- docs/myChart/metadata.js
+++ docs/myChart/metadata.js
@@ -1,8 +1,8 @@
 export const metadata = {
-  name: 'My Amazing Chart',
-  id: 'rawgraphs.myChart001',
-  category: 'Dispersions, Propotions',
-  description: 'a very simple scatterplot',
-  code: 'https://observablehq.com/',
+  name: 'Gantt Chart',
+  id: 'rawgraphs.ganttChart001',
+  category: 'Timelines',
+  description: 'A Gantt chart for visualizing project timelines',
+  code: 'https://github.com/your-repo-link',
   tutorial: 'https://rawgraphs.io/learning/',
 }
